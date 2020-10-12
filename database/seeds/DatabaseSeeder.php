3d658a312225d81dc57ccf7e5d4b109f1988a8bd
Pc-casa <jchavez666@gmail.com>
2020-10-12 07:54:29 +0300

    Blog Post, Category, Tag, User

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        // TODO: damos de alta todos los Seedeer
+        $this->call(UsersTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(TagsTableSeeder::class);
+        $this->call(PostsTableSeeder::class);
+    }
+}
